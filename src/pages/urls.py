ae04af8b06db061e2d3642abcd72846f437a5154
Bryan Rojas <bryroj14@gmail.com>
2019-03-12 15:17:10 -0700

    Updated urls with all links. Need to create views

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -4,6 +4,11 @@ from .views import HomePageView, BlogView, AboutUsView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('about-us/', AboutUsView.as_view(), name='about-us'),
     path('blog/', BlogView.as_view(), name='blog'),
-    path('about-us/', AboutUsView.as_view(), name='about-us')
+    path('project-documents/', ProjectDocumentView.as_view(), name='project-docs'),
+    path('legal/', LegalView.as_view(), name='legal'),
+    path('terms-of-service/', TermsOfServiceView.as_view(), name='tos'),
+    path('privacy-policy/', PrivacyPolicyView.as_view(), name='privacy-policy'),
+    path('credits/', CreditsView.as_view(), name='credits'),
 ]
