aec410d4c83343f4ea27b06a6b1ffe081b312785
Bryan Rojas <bryroj14@gmail.com>
2019-04-09 12:59:15 -0700

    Calorie Calculator filled out

--- src/pages/forms.py
+++ src/pages/forms.py
@@ -3,21 +3,32 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class MealPlanForm(forms.Form):
     diet_prefs_options = (
-        ('Any', 'any'),
-        ('Vegan', 'vegan'),
-        ('Vegetarian', 'vegetarian'),
-        ('Paleo', 'paleo'),
-        ('Miditerranean', 'miditerranean'),
-        ('Ketogenic', 'ketogenic'),
+        ('Any', 'Any'),
+        ('Vegan', 'Vegan'),
+        ('Vegetarian', 'Vegetarian'),
+        ('Paleo', 'Paleo'),
+        ('Miditerranean', 'Miditerranean'),
+        ('Ketogenic', 'Ketogenic'),
     )
+
     diet = forms.ChoiceField(choices = diet_prefs_options)
     calories = forms.IntegerField()
 
 class CalorieCalcForm(forms.Form):
     genders = (
-        ('Female', 'female'),
-        ('Male', 'male'),
-        ('Other', 'other'),
+        ('Female', 'Female'),
+        ('Male', 'Male'),
     )
+    exercise_levels = (
+        ('Little/No Exericse', 'Little/No Exericse'),
+        ('3-5 Times/Week', '3-5 Times/Week'),
+        ('Daily Exercise', 'Daily Exercise'),
+        ('Intense Daily Exercise', 'Intense Daily Exercise'),
+    )
+
     age = forms.IntegerField()
     gender = forms.ChoiceField(choices = genders)
+    current_weight = forms.IntegerField()
+    height_ft = forms.IntegerField()
+    height_inches = forms.IntegerField()
+    exercise_level = forms.ChoiceField(choices = exercise_levels)
