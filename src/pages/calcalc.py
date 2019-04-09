792bf972dff04bd7ff3bc01b2ae34f6c2d87c253
Bryan Rojas <bryroj14@gmail.com>
2019-04-08 18:47:04 -0700

    Placeholder for calorie calculator

--- src/pages/calcalc.py
+++ src/pages/calcalc.py
@@ -1,6 +1,10 @@
 def calorieCalulator(age, gender, weight, height):
     args = {}
 
+    height = height + 22222
+    weight = weight - 2
+    bmr = height + weight
+
     args['age'] = age
     args['gender'] = gender
     args['weight'] = weight
