904bec192a7e1534b24ff43dd61b9c08612dcf8c
Bryan Rojas <bryroj14@gmail.com>
2019-05-02 22:58:47 -0700

    Updated urls.py

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import HomePageView, AboutUsView, MeetTheTeamView, LegalView, TermsOfServiceView, PrivacyPolicyView, CreditsView, CalorieCalcView
 
 
 urlpatterns = [
